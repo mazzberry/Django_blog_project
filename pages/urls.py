706d2396aca7de70058076060634d973fab86c9f
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2023-11-19 05:43:54 +0330

    set shodan jadid url va views

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_url, name='home-page'),
+   # path('', views.homepage_url, name='home-page'),
     path('', views.blog, name='blog-page')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,16 +3,15 @@ from .models import Post
 
 # Create your views here.
 
-def homepage_url(request):
-    context={
-        'page-id':'home page',
-    }
-    return render(request, '_base.html',context)
+#def homepage_url(request):
+#    context={
+#        'page-id':'home page',
+#    }
+#    return render(request, '_base.html',context)
 
 def blog(request):
     all_posts = Post.objects.all()
     context = {
         'all_p':all_posts
     }
-################# temp name nazashti
-    return render(request, 'blog.blog.html',context)
+    return render(request, 'blog/blog.html',context)
