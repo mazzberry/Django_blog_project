56082b9b6fad969d7b181c17266ff7a46309d9a1
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2023-11-20 08:09:35 +0330

    ezafe shodan postdt , view/url

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
    # path('', views.homepage_url, name='home-page'),
-    path('', views.blog, name='blog-page')
+    path('', views.blog, name='blog-page'),
+    path('<int:pk>/', views.post_detail_view , name='post-detail'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
 
-#def homepage_url(request):
-#    context={
-#        'page-id':'home page',
-#    }
-#    return render(request, '_base.html',context)
 
 def blog(request):
     all_posts = Post.objects.all()
     context = {
-        'all_p':all_posts
+        'posts':all_posts
     }
-    return render(request, 'blog/blog.html',context)
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail_view(request, pk):
+#    #find by given id
+    post = Post.objects.get(pk=pk)
+    print('id in url:', pk)
+
+    return render(request, 'blog/post_detail.html', {'post':post})
