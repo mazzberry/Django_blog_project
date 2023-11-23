bae64dec00dbee5dc42f2cc65f6bd08692769043
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2023-11-23 20:54:16 +0330

    test nevisi , view bedune bug

--- pages/tests.py
+++ pages/tests.py
@@ -1,8 +1,53 @@
 from django.test import TestCase
 from .models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 # Create your tests here.
 
-#class test_Post(TestCase):
-#    def setUp(self):
-#        self.ust = Post.objects.create()
+class Blog_Post_test(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username= 'test-user1')
+        self.post1 = Post.objects.create(
+            title= 'test title',
+            author= self.user ,
+            text= 'salam in yek matn test hast',
+            status= Post.STATUS_CHOICES[0],
+        )
+
+    def test_postlist_url(self):
+
+        response = self.client.get('/blog/')
+        self.assertEquals(response.status_code,200)
+
+        pass
+
+    def test_postlist_url(self):
+
+        response = self.client.get('/blog/')
+        self.assertEquals(response.status_code,200)
+
+        pass
+
+
+    def test_postlist_url_byname(self):
+
+        response = self.client.get(reverse('blog-page'))
+        self.assertEquals(response.status_code, 200)
+
+        pass
+
+    def test_title_postlist_view(self):
+        response = self.client.get(reverse('blog-page'))
+        self.assertContains(response, self.post1.title)
+
+    def test_post_detail_on_detail_page(self):
+        response = self.client.get(f'/blog/{self.post1.id}/')
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.text)
+
+
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -14,7 +15,13 @@ def blog(request):
 
 def post_detail_view(request, pk):
 #    #find by given id
-    post = Post.objects.get(pk=pk)
-    print('id in url:', pk)
+    post = get_object_or_404(Post, pk=pk)
+#     try:
+#         post = Post.objects.get(pk=pk)
+#         print('id in url:', pk)
+
+#     except Post.DoesNotExist :
+#         context = None
+#         print('ex')
 
     return render(request, 'blog/post_detail.html', {'post':post})
